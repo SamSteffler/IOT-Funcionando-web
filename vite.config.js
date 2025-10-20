fa66df5b7694ae1b09a47c468d0ed7eb81be4380
SamSteffler <samuel.steffler@acad.ufsm.br>
2025-10-20 08:59:22 -0300

    ajustado endereço base para deploy no GitHub pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/IOT-Funcionando-web/',
   plugins: [
     vue(),
     vueDevTools(),
@@ -14,5 +15,6 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+    
   },
 })
